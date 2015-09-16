256654f31e6d7192e8b03839d40ee08da7839e75
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2015-09-16 23:41:52 +0200

    scripts

--- linacsynoptic/__main__.py
+++ linacsynoptic/__main__.py
@@ -5,14 +5,19 @@ from taurus.external.qt import Qt
 from svgsynoptic.taurussynopticwidget import TaurusSynopticWidget
 
 
-qapp = Qt.QApplication([])
-sw = TaurusSynopticWidget()
+def main():
+    qapp = Qt.QApplication([])
+    sw = TaurusSynopticWidget()
 
-# We need to give the absolute path to the HTML file
-# because our webview is setup to load assets from the
-# svgsynoptic library's path.
-path = os.path.dirname(__file__)
-sw.setModel(os.path.join(path, "resources/index.html"))
+    # We need to give the absolute path to the HTML file
+    # because our webview is setup to load assets from the
+    # svgsynoptic library's path.
+    path = os.path.dirname(__file__)
+    sw.setModel(os.path.join(path, "resources/index.html"))
 
-sw.show()
-qapp.exec_()
+    sw.show()
+    qapp.exec_()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="taurusgui-linacsynoptic",
+    version="1.0.0",
+    description="Synoptic for the MAX IV linear accellerator.",
+    author="Johan Forsberg",
+    author_email="johan.forsberg@maxlab.lu.se",
+    license="GPLv3",
+    url="http://www.maxlab.lu.se",
+    packages=['linacsynoptic'],
+    package_data={'linacsynoptic': [
+        'resources/index.html',
+        'resources/images/*.svg',
+        'resources/images/*.png']},
+    entry_points={
+        'console_scripts': [
+            'ctlinacsynoptic = linacsynoptic.__main__:main'
+        ]
+    }
+)
