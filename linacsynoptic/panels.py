384dfc08a3d99b5b7de2879a9129b8020a468cdb
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2016-02-19 14:49:27 +0100

    minor panel stuff

--- linacsynoptic/panels.py
+++ linacsynoptic/panels.py
@@ -5,20 +5,19 @@ from taurus.qt.qtgui.panel import TaurusDevicePanel
 
 # various widgets for specific devices
 from magnetpanel import MagnetPanel
-from maxwidgets.panel import MotorPresets, TaurusFormExpanding
-from tgconf_beamviewer import panels
+from maxwidgets.panel import MotorPresets
 
-db = Database()
 
 CLASS_PANELS = {
     "Magnet": MagnetPanel,
-    "IORegister": MotorPresets,
-    "Motor": TaurusFormExpanding
+    # "IORegister": MotorPresets,
+    # "Motor": TaurusFormExpanding
 }
 
 
 def get_panel(device):
     "Return an appropriate panel for the given device"
+    db = Database()
     classname = db.get_class_for_device(device)
     if classname in CLASS_PANELS:
         return CLASS_PANELS[classname]
@@ -35,3 +34,15 @@ def camera_process(cam):
     # that tends to slow down the camera UI when there are many listeners.
     print "opening camera %s in subprocess", cam
     Popen(["python", "%s/limabeam.py" % panels.__path__[0], cam])
+
+
+
+if __name__ == "__main__":
+    import sys
+    from taurus.qt.qtgui.application import TaurusApplication
+    qapp = TaurusApplication([])
+    device = sys.argv[1]
+    widget = get_panel(device)()
+    widget.setModel(device)
+    widget.show()
+    qapp.exec_()
