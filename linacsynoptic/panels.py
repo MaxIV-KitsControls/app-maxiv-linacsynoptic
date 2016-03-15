61ad1c20ab32bf3428ef892f6f7ae80b88e21d92
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2016-03-15 14:01:46 +0100

    add valve and screen popups

--- linacsynoptic/panels.py
+++ linacsynoptic/panels.py
@@ -1,6 +1,6 @@
 from subprocess import Popen
 
-from PyTango import Database
+from PyTango import Database, DevState
 from taurus.qt.qtgui.panel import TaurusDevicePanel
 
 # various widgets for specific devices
@@ -8,10 +8,23 @@ from magnetpanel import MagnetPanel
 from maxwidgets.panel import MotorPresets
 from tgconf_beamviewer import panels
 
+from popup import CommandsWidgetPopup
+
+
+class ValvePopup(CommandsWidgetPopup):
+    "Quick open/close for vacuum valves"
+    commands = ("Open", DevState.OPEN), ("Close", DevState.CLOSE)
+
+class ScreenPopup(CommandsWidgetPopup):
+    "Quick movements for camera screens"
+    commands = ("MoveIn", DevState.INSERT), ("MoveOut", DevState.EXTRACT)
+
 
 CLASS_PANELS = {
     "Magnet": MagnetPanel,
     "IORegister": MotorPresets,
+    "VacuumValve": ValvePopup,
+    "CameraScreen": ScreenPopup
 }
 
 
@@ -28,13 +41,12 @@ def get_panel(device):
     return TaurusDevicePanel
 
 
-def camera_process(cam):
+def camera_process(cam): 
     """Start a camera gui panel in its own process"""
     # This is a (hopefully) temporary workaround to the Taurus polling problem
     # that tends to slow down the camera UI when there are many listeners.
     print "opening camera %s in subprocess", cam
     Popen(["python", "%s/limabeam.py" % panels.__path__[0], cam])
-
 
 
 if __name__ == "__main__":
