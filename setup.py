9a4baeaaf6dbefbeb501975be45b7a4e43e41f0f
Paul Bell <paubel@w-v-scilab-cc-0.maxiv.lu.se>
2016-12-16 04:44:46 -0500

    fix magnet labels in TR1 RT10986

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="taurusgui-linacsynoptic",
-    version="1.3.3",
+    version="1.3.4",
     description="Synoptic for the MAX IV linear accellerator.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
