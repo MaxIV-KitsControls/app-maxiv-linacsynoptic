f810ec82f0ef7a0dbba7d52c626edd6e77500a87
Paul Bell <paul.bell@maxlab.lu.se>
2017-01-31 11:06:27 +0100

    fixes to above

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="taurusgui-linacsynoptic",
-    version="1.3.5",
+    version="1.3.6",
     description="Synoptic for the MAX IV linear accellerator.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
