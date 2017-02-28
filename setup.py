9c4f1029499cf0cce9c43de7b1a8bd83ae4fb735
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-02-28 15:45:32 +0100

    v1.3.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="taurusgui-linacsynoptic",
-    version="1.3.7",
+    version="1.3.8",
     description="Synoptic for the MAX IV linear accellerator.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
