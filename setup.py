c59a924b9564f593c6a9db37120d3469bc432736
Antonio Bartalesi <79691763+ABartalesi@users.noreply.github.com>
2021-03-12 17:47:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'resources/images/*.svg',
         'resources/images/*.png']},
     data_files=[('/usr/share/applications',['maxiv-linac-synoptic.desktop'])],
-    install_requires = ['taurus', 'magnetpanel', 'svgsynoptic2']
+    install_requires = ['taurus', 'magnetpanel', 'svgsynoptic2'],
     entry_points={
         'console_scripts': [
             'ctlinacsynoptic = linacsynoptic.__main__:main'
